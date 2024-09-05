013df92d1161782ec72006a22f49c441003cf0d9
ConeNDev <conedeveloper007@gmail.com>
2024-09-06 00:02:43 +0200

    Added logout method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,4 +26,13 @@ class LoginController extends Controller
 
         return response()->json(['error'=>'Unauthorised'], 401);
     }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully.'
+        ]);
+    }
 }
