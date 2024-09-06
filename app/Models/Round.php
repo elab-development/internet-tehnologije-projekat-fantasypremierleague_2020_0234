7aadd5f413354cf0b5b486e31cb4471c9a8bb488
ConeNDev <conedeveloper007@gmail.com>
2024-09-06 08:51:13 +0200

    Added Fixture, Round and statistic models and migrations

---
+++ app/Models/Round.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Round extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['league_id', 'round_id', 'first_team', 'second_team'];
+}
---
+++ database/migrations/2024_09_06_064428_create_fixtures_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fixtures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('round_id')->constrained('rounds');
+            $table->foreignId('first_team')->constrained('teams');
+            $table->foreignId('second_team')->constrained('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fixtures');
+    }
+};
